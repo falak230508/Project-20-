e2dfc730652558b971ac3ab76ed21c004d8bf5e7
falak230508 <72301138+falak230508@users.noreply.github.com>
2020-10-20 18:22:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+//global variables
+var car , wall ;
+var speed , weight;
+
+
+function setup() {
+//creating the canvas  
+createCanvas(800,400);
+
+//giving random positon to weught and speed
+  speed = random(55,90);
+  weight = random(400,1500);
+
+//creating the car sprite
+car = createSprite(50, 200, 50, 50);
+car.velocityX = speed;
+car.shapeColor = "red";
+
+//creating the wall sprite
+wall = createSprite(600,200,60,height/2);
+wall.shapeColor ="blue";
+}
+
+function draw() {
+  background("pink");  
+
+if(wall.x - car.x < (car.width + wall.width)/2)
+{
+car.velocityX = 0;
+
+var deformation = 0.5 * weight * speed * speed/22509;
+if(deformation > 180)
+{
+car.shapeColor = color(255,0,0)
+}
+
+if(deformation < 180 && deformation > 100)
+{
+   car.shapeColor = color(230,230,0);
+}
+if(deformation < 100)
+{
+  car.shapeColor = color(0,255,0);
+}
+ }
+  drawSprites();
+}
